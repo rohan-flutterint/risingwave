ee3447a00348ca9bc259304ca837c6ac5bbbeb1b
Kexiang Wang <kx.wang@hotmail.com>
2023-01-12 16:32:23 -0500

    hack cpu count

--- src/compute/src/lib.rs
+++ src/compute/src/lib.rs
@@ -167,5 +167,6 @@ fn default_total_memory_bytes() -> usize {
 }
 
 fn default_parallelism() -> usize {
-    total_cpu_available().ceil() as usize
+    // total_cpu_available().ceil() as usize
+    1
 }
