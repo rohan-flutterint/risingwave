babb9ccef728f3a20f5fa1f96fecc7232c1b4a08
Peng Chen <peng@singularity-data.com>
2023-01-15 22:18:36 +0800

    reformat
    Signed-off-by: Peng Chen <peng@singularity-data.com>

--- src/rpc_client/src/meta_client.rs
+++ src/rpc_client/src/meta_client.rs
@@ -12,10 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::borrow::BorrowMut;
 use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
-use std::sync::{mpsc, Arc};
+use std::sync::Arc;
 use std::time::Duration;
 
 use anyhow::{anyhow, Context};
